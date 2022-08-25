6fe557d12e4fbb1f180b3f91f39ddd883c4deaca
Katherine Graf <katherinelgraf@gmail.com>
2022-08-25 11:49:58 -0500

    cleanup commit

---
+++ src/main/kotlin/com/kg/todo/models/Points.kt
@@ -0,0 +1,11 @@
+package com.kg.todo.models
+
+import java.time.LocalDateTime
+import java.util.*
+
+class Points (
+
+    val payer: String,
+    val points: Int,
+    val timestamp: LocalDateTime
+)
--- src/main/kotlin/com/kg/todo/utils/Settings.kt
+++ src/main/kotlin/com/kg/todo/utils/Settings.kt
@@ -1,5 +1,11 @@
 package com.kg.todo.utils
 
+import com.kg.todo.models.Points
+import java.util.*
+
 val STATUS_INCOMPLETE: String = "incomplete"
 val STATUS_COMPLETED: String = "completed"
 val STATUS_CANCELLED: String = "cancelled"
+
+//var current_points: MutableList<Points> = emptyList<Points>() as MutableList<Points>
+//var list = mutableListOf<Points>()
