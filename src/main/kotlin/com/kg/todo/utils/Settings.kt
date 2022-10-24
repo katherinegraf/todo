20befc1f79a7a3326a1615260ffe5b953b2ee622
Katherine Graf <61607113+katherinegraf@users.noreply.github.com>
2022-10-24 12:09:26 -0500

    PER-2 Remove rewards code (#11)
    * rename STATUS_INCOMPLETE to STATUS_ACTIVE
    
    * cleanup unrelated code

--- src/main/kotlin/com/kg/todo/utils/Settings.kt
+++ src/main/kotlin/com/kg/todo/utils/Settings.kt
@@ -1,11 +1,5 @@
 package com.kg.todo.utils
 
-import com.kg.todo.models.Points
-import java.util.*
-
 val STATUS_ACTIVE: String = "active"
 val STATUS_COMPLETED: String = "completed"
 val STATUS_CANCELLED: String = "cancelled"
-
-//var current_points: MutableList<Points> = emptyList<Points>() as MutableList<Points>
-//var list = mutableListOf<Points>()
